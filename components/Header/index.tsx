291e8fdf3252505049bb7d6dce7f3ecf631b1bb1
heyijian <heyijian@zhuanzhuan.com>
2024-12-31 15:02:21 +0800

    feat(components): 更新 Header 组件并添加新功能
    - 新增更新日志功能，记录工具版本更新和功能变更
    - 增加版本号至 V1.0.1
    - 添加新工具和功能：
      - 字体分析工具
      - JSON 工具，支持字段/字段值搜索
      - 工程/二维码工具栏
      - 工程类型筛选和搜索框

--- components/Header/index.tsx
+++ components/Header/index.tsx
@@ -5,18 +5,26 @@ import { openWindow } from "~components/Body/components/Matching/utils"
 
 import styles from "./index.module.css"
 
+const updateLog = `
+2024.12.31（V1.0.1）：新增字体分析、JSON 工具，JSON 工具目前支持对 JSON 原文进行【字段/字段值搜索】，可以从复杂的 JSON 原文中快速获取字段/字段值级联关系
+2024.12.28（V1.0.0）：新增工程/二维码工具栏，为工程添加类型筛选，添加搜索框
+`
+
 const Header: FC = () => {
   return (
     <>
       <div className={styles.container}>
         <div className={styles.title}>zzChromeTools</div>
         <div style={{ display: "flex", alignItems: "center", gap: "8px" }}>
-          <div className={styles.version}>V0.0.1</div>
           <div
             onClick={() => openWindow("http://10.238.52.99:8000")}
             style={{ cursor: "pointer" }}>
             管理中心
           </div>
+          <div onClick={() => alert(updateLog)} style={{ cursor: "pointer" }}>
+            更新日志
+          </div>
+          <div className={styles.version}>V1.0.1</div>
         </div>
       </div>
     </>
