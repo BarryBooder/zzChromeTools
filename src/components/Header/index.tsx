b5dccca57d4298ac1a59a843cc36efd82b9ab220
BarryBooder <heyijian@live.cn>
2025-01-09 18:48:33 +0800

    feat(components): 更新 Header 组件版本号并添加更新日志
    - 在 Header 组件中添加 2025.01.09（V1.0.4）版本的更新日志
    - 将版本号从 V1.0.2 修改为 V1.0.4
    - 新增埋点监控工具的筛选功能和页面加载前注入脚本能力

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -5,6 +5,7 @@ import { openWindow } from "../Body/components/Matching/utils"
 import styles from "./index.module.css"
 
 const updateLog = `
+2025.01.09（V1.0.4）：埋点监控工具提供筛选功能，并可以在页面加载前注入脚本，实现全埋点拦截
 2025.01.08（V1.0.3）：将埋点监控工具移至控制台，调整注入方式，二维码工具提供增大/减小尺寸，以方便密集二维码加大展示
 2025.01.06（V1.0.2）：新增埋点监控工具，可以快捷查看埋点拦截，方便前端进行埋点验证
 2024.12.31（V1.0.1）：新增字体分析、JSON 工具，JSON 工具目前支持对 JSON 原文进行【字段/字段值搜索】，可以从复杂的 JSON 原文中快速获取字段/字段值级联关系
@@ -25,7 +26,7 @@ const Header: FC = () => {
           <div onClick={() => alert(updateLog)} style={{ cursor: "pointer" }}>
             更新日志
           </div>
-          <div className={styles.version}>V1.0.2</div>
+          <div className={styles.version}>V1.0.4</div>
         </div>
       </div>
     </>
